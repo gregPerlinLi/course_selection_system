e410072ec0819420b3b3c87a9f5ef37a7f44365d
gregPerlinLi <lihaolin13@outlook.com>
2021-07-21 18:13:24 +0800

    fix(): 修正POJO对象名称以防冲突

---
+++ src/main/java/com/gregperlinli/pojo/Classes.java
@@ -0,0 +1,65 @@
+package com.gregperlinli.pojo;
+
+/**
+ * 用于对应数据库中的class表
+ *
+ * @author gregperlinli
+ */
+public class Classes {
+    private int id;
+    private String classesName;
+    private String college;
+    private String grade;
+
+    public Classes() {
+    }
+
+    public Classes(int id, String classesName, String college, String grade) {
+        this.id = id;
+        this.classesName = classesName;
+        this.college = college;
+        this.grade = grade;
+    }
+
+    public String getClassesName() {
+        return classesName;
+    }
+
+    public void setClassesName(String classesName) {
+        this.classesName = classesName;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Classes{" +
+                "id=" + id +
+                ", className='" + classesName + '\'' +
+                ", college='" + college + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
