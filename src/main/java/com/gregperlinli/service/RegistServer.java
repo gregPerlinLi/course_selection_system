7b2608e93fbef3548e700b95d9523e38e321077d
gregPerlinLi <lihaolin13@outlook.com>
2021-07-26 17:04:11 +0800

    feat(): 新增注册模块

--- src/main/java/com/gregperlinli/service/RegistServer.java
+++ src/main/java/com/gregperlinli/service/RegistServer.java
@@ -1,4 +1,27 @@
 package com.gregperlinli.service;
 
+import com.gregperlinli.pojo.Admin;
+import com.gregperlinli.pojo.Student;
+
+/**
+ * 用于进行注册操作
+ *
+ * @author gregperlinli
+ */
 public interface RegistServer {
+    /**
+     * 普通学生的注册操作
+     *
+     * @param student 提供需要注册的学生信息
+     * @return 返回知否已存在该用户，<code>true</code>则为不存在同名/同号用户，<code>false</code>则为存在同名/同号用户
+     */
+    boolean studentRegist(Student student);
+
+    /**
+     * 普通学生的注册操作
+     *
+     * @param admin 提供需要注册的管理员信息
+     * @return 返回知否已存在该用户，<code>true</code>则为不存在同名/同号用户，<code>false</code>则为存在同名/同号用户
+     */
+    boolean adminRegist(Admin admin);
 }
