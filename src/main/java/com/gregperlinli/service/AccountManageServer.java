7ddd08ccc817d2ea169ad1715fac4d65a4a68f9d
gregPerlinLi <lihaolin13@outlook.com>
2021-07-27 17:08:11 +0800

    fix(): 修改JavaDoc注释
    将注释中的“普通学生”改为“管理员”

--- src/main/java/com/gregperlinli/service/AccountManageServer.java
+++ src/main/java/com/gregperlinli/service/AccountManageServer.java
@@ -19,6 +19,7 @@ public interface AccountManageServer {
 
     /**
      * 普通学生的注册操作
+     * 管理员的注册操作
      *
      * @param admin 提供需要注册的管理员信息
      * @return 返回知否已存在该用户，<code>true</code>则为不存在同名/同号用户，<code>false</code>则为存在同名/同号用户
