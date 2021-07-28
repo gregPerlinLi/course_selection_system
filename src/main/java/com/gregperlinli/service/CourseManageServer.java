80a9982612ebbf86e43bd13812b86213a99aa519
gregPerlinLi <lihaolin13@outlook.com>
2021-07-28 18:03:13 +0800

    fix(): 完善JavaDoc
    完善JavaDoc并加入修改/删除时将会一并删除其下的所有已选课程信息的提示

--- src/main/java/com/gregperlinli/service/CourseManageServer.java
+++ src/main/java/com/gregperlinli/service/CourseManageServer.java
@@ -11,7 +11,21 @@ public interface CourseManageServer {
      */
     boolean addCourse(Course course);
 
+    /**
+     * 修改课程操作<br>
+     * <span style='color:yellow;'>注意：在修改课程的同时会将原来该课程下的所有已选课程信息完全清除，请谨慎使用！</span>
+     *
+     * @param course 提供带有要修改的课程的新的参数的对象 (注意：id属性一定是课程现有的id属性，且不能更改！)
+     * @return 返回知否已存在该课程，<code>true</code>则为存在该课程，<code>false</code>则为不存在该课程
+     */
     boolean updateCourse(Course course);
 
+    /**
+     * 删除课程操作<br>
+     * <span style='color:yellow;'>注意：在删除课程的同时会将原来该课程下的所有已选课程信息完全清除，请谨慎使用！</span>
+     *
+     * @param id 提供要删除的课程id
+     * @return 返回知否已存在该课程，<code>true</code>则为存在该课程，<code>false</code>则为不存在该课程
+     */
     boolean deleteCourse(int id);
 }
