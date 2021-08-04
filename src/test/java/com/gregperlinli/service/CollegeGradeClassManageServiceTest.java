a331ce804478d44b3f56b58a8fbcc8eedce2788a
gregPerlinLi <lihaolin13@outlook.com>
2021-08-04 18:55:09 +0800

    feat(): 对学院/年级/班级管理模块进行测试
    对学院/年级/班级管理模块新增的更改/删除方法进行单元测试

--- src/test/java/com/gregperlinli/service/CollegeGradeClassManageServiceTest.java
+++ src/test/java/com/gregperlinli/service/CollegeGradeClassManageServiceTest.java
@@ -46,4 +46,71 @@ public class CollegeGradeClassManageServiceTest {
         System.out.println(isAdded);
     }
 
+    /**
+     * 测试<code>collegeUpdate()</code>方法
+     */
+    @Test
+    public void testCollegeUpdate() {
+        final College college = new College(2, "物电学院");
+        final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+        boolean isUpdate = cgcms.collegeUpdate(college);
+        System.out.println(isUpdate);
+    }
+
+
+    /**
+     * 测试<code>gradeUpdate()</code>方法
+     */
+    @Test
+    public void testGradeUpdate() {
+        final Grade grade = new Grade(2, "19级");
+        final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+        boolean isUpdate = cgcms.gradeUpdate(grade);
+        System.out.println(isUpdate);
+    }
+
+
+    /**
+     * 测试<code>classUpdate()</code>方法
+     */
+    @Test
+    public void testClassUpdate() {
+        final Classes classes = new Classes(3,"20电子科学与技术3班", "物理与光电工程学院", "20级");
+        final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+        boolean isUpdate = cgcms.classUpdate(classes);
+        System.out.println(isUpdate);
+    }
+
+    /**
+     * 测试<code>collegeDelete()</code>方法
+     */
+    @Test
+    public void testCollegeDelete() {
+        final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+        boolean isDelete = cgcms.collegeDelete(5);
+        System.out.println(isDelete);
+    }
+
+
+    /**
+     * 测试<code>gradeDelete()</code>方法
+     */
+    @Test
+    public void testGradeDelete() {
+        final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+        boolean isDelete = cgcms.gradeDelete(5);
+        System.out.println(isDelete);
+    }
+
+
+    /**
+     * 测试<code>classDelete()</code>方法
+     */
+    @Test
+    public void testClassDelete() {
+        final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+        boolean isDelete = cgcms.classDelete(12);
+        System.out.println(isDelete);
+    }
+
 }
