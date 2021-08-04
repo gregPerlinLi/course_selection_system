7776e597c21235be392effd3e13afa0d8b1edbe5
gregPerlinLi <lihaolin13@outlook.com>
2021-08-04 18:54:28 +0800

    feat(): 新增学院/年级/班级的修改/删除方法
    在学院/年级/班级管理模块新增了修改/删除方法

--- src/main/java/com/gregperlinli/service/CollegeGradeClassManageService.java
+++ src/main/java/com/gregperlinli/service/CollegeGradeClassManageService.java
@@ -36,16 +36,55 @@ public interface CollegeGradeClassManageService {
      */
     boolean classInsert(Classes classes);
 
+    /**
+     * 修改学院操作<br/>
+     * <span style='color:yellow;'>注意：在修改学院的同时会将原来该学院下的所有学生以及班级信息进行修改</span>
+     *
+     * @param college 提供带有要修改的学院的新的参数的对象 (注意：id属性一定是学院现有的id属性，且不能更改！)
+     * @return 返回知否已存在该学院，<code>true</code>则为存在该学院，<code>false</code>则为不存在该学院
+     */
     boolean collegeUpdate(College college);
 
+    /**
+     * 修改年级操作<br/>
+     * <span style='color:yellow;'>注意：在修改年级的同时会将原来该年级下的所有学生以及班级信息进行修改</span>
+     *
+     * @param grade 提供带有要修改的年级的新的参数的对象 (注意：id属性一定是年级现有的id属性，且不能更改！)
+     * @return 返回知否已存在该年级，<code>true</code>则为存在该年级，<code>false</code>则为不存在该年级
+     */
     boolean gradeUpdate(Grade grade);
 
+    /**
+     * 修改班级操作<br/>
+     * <span style='color:yellow;'>注意：在修改班级的同时会将原来该班级下的所有学生信息进行修改</span>
+     *
+     * @param classes 提供带有要修改的班级的新的参数的对象 (注意：id属性一定是班级现有的id属性，且不能更改！)
+     * @return 返回知否已存在该班级，<code>true</code>则为存在该班级，<code>false</code>则为不存在该班级
+     */
     boolean classUpdate(Classes classes);
 
+    /**
+     * 删除学院操作<br>
+     * <span style='color:yellow;'>注意：在删除学院的同时会将原来该学院下的所有学生以及班级信息完全清除，请谨慎使用！</span>
+     * @param id 提供要删除的学院id
+     * @return 返回知否已存在该学院，<code>true</code>则为存在该学院，<code>false</code>则为不存在该学院
+     */
     boolean collegeDelete(int id);
 
+    /**
+     * 删除年级操作<br>
+     * <span style='color:yellow;'>注意：在删除年级的同时会将原来该年级下的所有学生以及班级信息完全清除，请谨慎使用！</span>
+     * @param id 提供要删除的年级id
+     * @return 返回知否已存在该年级，<code>true</code>则为存在该年级，<code>false</code>则为不存在该年级
+     */
     boolean gradeDelete(int id);
 
+    /**
+     * 删除班级操作<br>
+     * <span style='color:yellow;'>注意：在删除班级的同时会将原来该班级下的所有学生信息完全清除，请谨慎使用！</span>
+     * @param id 提供要删除的班级id
+     * @return 返回知否已存在该班级，<code>true</code>则为存在该班级，<code>false</code>则为不存在该班级
+     */
     boolean classDelete(int id);
 
 }
