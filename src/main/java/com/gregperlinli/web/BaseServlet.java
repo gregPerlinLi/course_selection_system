b046ef3638952bf2f29a17d566b794028673fc5a
gregPerlinLi <lihaolin13@outlook.com>
2021-08-06 21:36:42 +0800

    feat(): 建立基础控制器
    建立登录和注册用的基础控制器

---
+++ src/main/java/com/gregperlinli/web/BaseServlet.java
@@ -0,0 +1,50 @@
+package com.gregperlinli.web;
+
+import com.gregperlinli.service.AccountManageService;
+import com.gregperlinli.service.CollegeGradeClassManageService;
+import com.gregperlinli.service.LoginService;
+import com.gregperlinli.service.impl.AccountManageServiceImpl;
+import com.gregperlinli.service.impl.CollegeGradeClassManageServiceImpl;
+import com.gregperlinli.service.impl.LoginServiceImpl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * @author gregperlinli
+ */
+@WebServlet(name = "BaseServlet", value = "/BaseServlet")
+public abstract class BaseServlet extends HttpServlet {
+    protected final LoginService loginService = new LoginServiceImpl();
+    protected final AccountManageService accountManageService = new AccountManageServiceImpl();
+    protected final CollegeGradeClassManageService cgcms = new CollegeGradeClassManageServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        try {
+            // Through the action business identification string, get the corresponding business method reflection
+            Method method = this.getClass().getDeclaredMethod(action, HttpServletRequest.class, HttpServletResponse.class);
+            // Call target business method
+            method.invoke(this, request, response);
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        try {
+            // Through the action business identification string, get the corresponding business method reflection
+            Method method = this.getClass().getDeclaredMethod(action, HttpServletRequest.class, HttpServletResponse.class);
+            // Call target business method
+            method.invoke(this, request, response);
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+}
