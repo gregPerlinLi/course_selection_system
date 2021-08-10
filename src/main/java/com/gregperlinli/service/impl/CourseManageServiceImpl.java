8a8d1d72f1cfb81e4d257d47fbd297aa4162c030
gregPerlinLi <lihaolin13@outlook.com>
2021-08-10 17:02:57 +0800

    fix(): 提取CourseDao
    提取CourseDao以减少代码量

--- src/main/java/com/gregperlinli/service/impl/CourseManageServiceImpl.java
+++ src/main/java/com/gregperlinli/service/impl/CourseManageServiceImpl.java
@@ -19,11 +19,11 @@ import java.util.List;
  * @since 2021-7-28
  */
 public class CourseManageServiceImpl implements CourseManageService {
-    Connection conn = null;
+    final private CourseDao courseDao = new CourseDaoImpl();
+    private Connection conn = null;
 
     @Override
     public boolean addCourse(Course course) {
-        final CourseDao courseDao = new CourseDaoImpl();
         try {
             conn = JDBCUtils.getConnectionWithPool();
             if ( courseDao.getCourseByCourseName(conn, course.getCourseName()) == null) {
@@ -40,7 +40,6 @@ public class CourseManageServiceImpl implements CourseManageService {
 
     @Override
     public boolean updateCourse(Course course) {
-        final CourseDao courseDao = new CourseDaoImpl();
         final SelectedCourseDao selectedCourseDao = new SelectedCourseDaoImpl();
         try {
             conn = JDBCUtils.getConnectionWithPool();
@@ -84,7 +83,6 @@ public class CourseManageServiceImpl implements CourseManageService {
 
     @Override
     public boolean deleteCourse(int id) {
-        final CourseDao courseDao = new CourseDaoImpl();
         final SelectedCourseDao selectedCourseDao = new SelectedCourseDaoImpl();
         try {
             conn = JDBCUtils.getConnectionWithPool();
