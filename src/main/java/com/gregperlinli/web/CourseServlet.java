ee292fac581f5006ee4e24939373534ec05c154d
gregPerlinLi <lihaolin13@outlook.com>
2021-08-12 20:24:05 +0800

    feat(): 添加JavaDoc文档注释
    添加JavaDoc文档注释

--- src/main/java/com/gregperlinli/web/CourseServlet.java
+++ src/main/java/com/gregperlinli/web/CourseServlet.java
@@ -28,6 +28,19 @@ public class CourseServlet extends BaseServlet {
     protected final CourseManageService courseManageService = new CourseManageServiceImpl();
     protected final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 
+
+    /**
+     * 处理添加课程功能<br/>
+     * 若添加成功会重定向到<code>course_management.html</code>，若失败则继续停留在该界面
+     *
+     * @param request 添加请求，需要通过POST请求提供课程名<code>courseName</code>，
+     *                开始选课日期<code>startDate</code>，
+     *                开始选课日期<code>startTime</code>，
+     *                限选人数<code>maxStu</code>
+     * @param response 添加响应
+     * @throws ServletException 抛出错误
+     * @throws IOException 抛出错误
+     */
     protected void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String courseName = request.getParameter("courseName");
@@ -50,10 +63,18 @@ public class CourseServlet extends BaseServlet {
         } else {
             // 添加失败
             System.out.println("The course [ " + course.getCourseName() + " ] is already exist!");
-            request.getRequestDispatcher("/pages/admin/add_course.html").forward(request, response);
+            response.sendRedirect(request.getContextPath() + "/pages/admin/add_course.html");
         }
     }
 
+    /**
+     * 通过Ajax请求处理删除课程功能
+     *
+     * @param request 删除请求，要在其中输入一个需要删除的课程<coed>id</coed>
+     * @param response 删除响应，将会返回一个布尔值<code>isDeleted</code>，若为<code>True</code>，则成功删除，若为<code>false</code>，则删除失败
+     * @throws ServletException 抛出错误
+     * @throws IOException 抛出错误
+     */
     protected void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 从request中获取要删除的id值
         int id = Integer.parseInt(request.getParameter("id"));
@@ -69,10 +90,54 @@ public class CourseServlet extends BaseServlet {
         response.getWriter().write(json);
     }
 
+    /**
+     * 处理修改课程功能
+     * 若修改成功会重定向到<code>course_management.html</code>，若失败则继续停留在该界面
+     *
+     * @param request 修改请求，需要通过POST请求提供要修改的课程<code>id</code>，
+     *                课程名<code>courseName</code>，
+     *                开始选课日期<code>startDate</code>，
+     *                开始选课日期<code>startTime</code>，
+     *                限选人数<code>maxStu</code>
+     * @param response 修改响应
+     * @throws ServletException 抛出错误
+     * @throws IOException 抛出错误
+     */
     protected void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        int id = Integer.parseInt(request.getParameter("id"));
+        String courseName = request.getParameter("courseName");
+        Date startDate = Date.valueOf(request.getParameter("startDate"));
+        String startTimeStr = request.getParameter("startTime");
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        java.util.Date d = null;
+        try {
+            d = format.parse(startTimeStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        java.sql.Time startTime = new java.sql.Time(d.getTime());
+        Integer maxStu = Integer.parseInt(request.getParameter("maxStu"));
+        Course course = new Course(id, courseName, startDate, startTime, maxStu);
+
+        if ( courseManageService.updateCourse(course) ) {
+            // 添加成功
+            response.sendRedirect(request.getContextPath() + "/pages/admin/course_management.html");
+        } else {
+            // 添加失败
+            System.out.println("The course [ " + course.getCourseName() + " ] is already exist!");
+            response.sendRedirect(request.getContextPath() + "/pages/admin/update_course.html?id=" + course.getId());
+        }
     }
 
+    /**
+     * 通过Ajax请求获取某个<code>id</code>对应的课程信息
+     *
+     * @param request 请求，要在其中输入一个需要获取的课程<code>id</code>
+     * @param response 响应，将会返回一个包含所查找的课程信息的对象<code>course</code>
+     * @throws ServletException 抛出错误
+     * @throws IOException 抛出错误
+     */
     protected void getCourse(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
         Course course = courseManageService.getCourseById(id);
@@ -84,6 +149,14 @@ public class CourseServlet extends BaseServlet {
 
     }
 
+    /**
+     * 通过Ajax请求获得所有课程信息
+     *
+     * @param request 请求
+     * @param response 响应，将会返回一个包含所有课程信息的对象集合<code>allCourse</code>
+     * @throws ServletException 抛出错误
+     * @throws IOException 抛出错误
+     */
     protected void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 1. 通过CourseManageService查询全部课程
         List<Course> allCourse = courseManageService.getAllCourse();
@@ -94,6 +167,14 @@ public class CourseServlet extends BaseServlet {
         response.getWriter().write(json);
     }
 
+    /**
+     * 通过Ajax请求检查是否存在该课程名
+     *
+     * @param request 请求，要在其中输入一个需要检查的课程名参数<code>courseName</code>
+     * @param response 响应，将会返回一个布尔值<code>existCourseName</code>，若为<code>True</code>，则已存在该课程名，若为<code>false</code>，则不存在该课程名
+     * @throws ServletException 抛出错误
+     * @throws IOException 抛出错误
+     */
     protected void existCourseName(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String courseName = request.getParameter("courseName");
 
