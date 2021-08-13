a2f39fad851704292cbd9729dec6de762284a8eb
gregPerlinLi <lihaolin13@outlook.com>
2021-08-13 17:33:35 +0800

    fix(): 提取函数
    提取从URL中获取请求参数的函数，以减少代码量

--- src/main/webapp/static/js/update_course.js
+++ src/main/webapp/static/js/update_course.js
@@ -68,13 +68,6 @@ $(function () {
         }
     }
 
-    function getQueryString(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) return unescape(r[2]);
-        return null;
-    }
-
     $("#courseName").blur(function () {
         var courseName = this.value;
         var infoObj = $("#courseNameInfo");
