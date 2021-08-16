a6e0bfb9a1c6a33ddf0afff164000a0403c6450b
gregPerlinLi <lihaolin13@outlook.com>
2021-08-16 18:22:42 +0800

    feat(): 添加查看已选课程信息界面
    添加学生查看自己已选课程信息界面

---
+++ src/main/webapp/static/js/selected_course.js
@@ -0,0 +1,26 @@
+$(function () {
+    var ajaxUrl = "http://localhost:8080/course_selection_system/courseSelectionServlet";
+
+    $.getJSON(ajaxUrl, "action=getSelectedCourse&charset=utf-8", function (data) {
+        console.log(data);
+        var outputTable = "";
+        $.each(data, function (index, selectedCoureses) {
+            outputTable += "<tr id='tr_" + selectedCoureses.id + "' >" +
+                "<td>" + selectedCoureses.id + "</td>" +
+                "<td>" + selectedCoureses.stuNum + "</td>" +
+                "<td>" + selectedCoureses.stuName + "</td>" +
+                "<td>" + selectedCoureses.course + "</td>" +
+                "<td><button class='cancel' data-id='" + selectedCoureses.id +"'>退选</button></td>" +
+                "</tr>";
+        });
+        $("#courseList").append(outputTable);
+        $("#title").prepend($("#account").html());
+    });
+
+    $(document).on("click", ".cancel", function () {
+       if ( confirm("确定退选此课程吗？") ){
+           var id = $(this).attr("data-id");
+           alert("退选id为" + id + "的课程");
+       }
+    });
+});
