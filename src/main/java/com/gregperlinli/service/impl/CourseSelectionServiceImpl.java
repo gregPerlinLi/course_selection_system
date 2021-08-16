86ad259250291793e735281b1169c28e2d107310
gregPerlinLi <lihaolin13@outlook.com>
2021-08-16 15:58:54 +0800

    feat(): 新增获取开选课程方法
    新增获取所有开选课程的方法

--- src/main/java/com/gregperlinli/service/impl/CourseSelectionServiceImpl.java
+++ src/main/java/com/gregperlinli/service/impl/CourseSelectionServiceImpl.java
@@ -1,3 +1,22 @@
+package com.gregperlinli.service.impl;
+
+import com.gregperlinli.dao.CourseDao;
+import com.gregperlinli.dao.SelectedCourseDao;
+import com.gregperlinli.dao.impl.CourseDaoImpl;
+import com.gregperlinli.dao.impl.SelectedCourseDaoImpl;
+import com.gregperlinli.pojo.Course;
+import com.gregperlinli.pojo.SelectedCourse;
+import com.gregperlinli.service.CourseSelectionService;
+import com.gregperlinli.utils.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <code>CourseSelectionService</code>的实现类
  *
@@ -22,5 +41,26 @@ public class CourseSelectionServiceImpl implements CourseSelectionService {
 
     @Override
     public List<Course> queryEnableCourse() {
+        try {
+            conn = JDBCUtils.getConnectionWithPool();
+            List<Course> courses = courseDao.getAll(conn);
+            List<Course> enabledCourses = new ArrayList<>();
+            for ( Course course : courses ) {
+                Date sqlStartDate = course.getStartDate();
+                Time sqlStartTime = course.getStartTime();
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                LocalDateTime localStartDateTime = LocalDateTime.parse(sqlStartDate.toString() + " " + sqlStartTime.toString(), dtf);
+                LocalDateTime localCurrentDateTime = LocalDateTime.now();
+                if ( localCurrentDateTime.isAfter(localStartDateTime) || localCurrentDateTime.isEqual(localStartDateTime) ) {
+                    enabledCourses.add(course);
+                }
+            }
+            return enabledCourses;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+        return null;
     }
 }
