a5cd13e5b1e9dfd7e25bf94e510cd27e83b3ff05
gregPerlinLi <lihaolin13@outlook.com>
2021-08-16 18:21:40 +0800

    feat(): 添加学生选课界面
    添加学生选课界面

---
+++ src/main/webapp/static/js/course_selection.js
@@ -0,0 +1,27 @@
+$(function () {
+    var ajaxUrl = "http://localhost:8080/course_selection_system/courseSelectionServlet";
+
+    $.getJSON(ajaxUrl, "action=list&charset=utf-8", function (data) {
+        console.log(data);
+        var outputTable = "";
+        $.each(data, function (index, enabledCourse) {
+            outputTable += "<tr id='tr_" + enabledCourse.id + "' >" +
+                "<td>" + enabledCourse.id + "</td>" +
+                "<td>" + enabledCourse.courseName + "</td>" +
+                "<td>" + enabledCourse.startDate + "</td>" +
+                "<td>" + enabledCourse.startTime + "</td>" +
+                "<td>" + enabledCourse.maxStu + "</td>" +
+                "<td>" + enabledCourse.currentStu + "</td>" +
+                "<td><button class='select' data-id='" + enabledCourse.id +"'>选课</button></td>" +
+                "</tr>";
+        });
+        $("#courseList").append(outputTable);
+    });
+
+    $(document).on("click", ".select", function () {
+       if ( confirm("确定选择此课程？") ) {
+           var id = $(this).attr("data-id");
+           alert("已选择id为" + id + "的课程");
+       } 
+    });
+});
